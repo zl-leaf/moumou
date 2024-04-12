5d130bffc6832d5da9311080b8ec4422f19cb2c6
zl-leaf <cc.leaf92@gmail.com>
2024-04-12 22:54:11 +0800

    feat: 修复请求错误的问题

--- view/src/api/request.ts
+++ view/src/api/request.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useUserStore } from "@/pinia/modules/user"
 
 export const http = axios.create({
-    baseURL: 'http://127.0.0.1:8888',
+    baseURL: import.meta.env.VITE_BASE_API,
     timeout: 99999,
 })
 
