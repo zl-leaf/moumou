267d48bbe5c5140829ba156cbb4b326c1cf07555
zl-leaf <cc.leaf92@gmail.com>
2024-04-21 20:47:22 +0800

    feat: view的构建脚本

---
+++ view/build.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# 创建构建目录
+build_server() {
+  bin_path=$1"/dist"
+  if [ ! -d "$bin_path" ]; then
+    mkdir -p $bin_path
+  fi
+
+  export VITE_APP_PATH=$bin_path && npm run build
+}
+
+shell_path=$(realpath $0)
+project_root=$(dirname "$shell_path")
+cd $project_root
+bin_root=$project_root
+while [[ "$1" != "" ]]
+do
+  case $1 in
+    --bin )
+      bin_root=$2
+      shift 2
+      ;;
+    * )
+      break
+      ;;
+  esac
+done
+
+build_server $bin_root
--- view/vite.config.ts
+++ view/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: path.resolve(__dirname, process.env.VITE_APP_PATH || 'dist')
   }
 })
