b5a8cde3c8246e4b4337eca3421cd567d2afab28
zl-leaf <cc.leaf92@gmail.com>
2026-01-27 21:41:51 +0800

    调整样式

--- view/src/views/manage/article/Add.vue
+++ view/src/views/manage/article/Add.vue
@@ -1,7 +1,7 @@
 <template>
     <ContentPage>
         <template #content>
-            <a-form :model="formState" :label-col="{ span: 4 }" :wrapper-col="{ span: 12 }">
+            <a-form :model="formState" :label-col="{ span: 6 }" :wrapper-col="{ span: 8 }">
                 <a-form-item
                     label="标题"
                     :rules="[{ required: true, message: '请输入标题' }]"
@@ -9,17 +9,7 @@
                     <a-input v-model:value="formState.title" />
                 </a-form-item>
 
-                <a-form-item
-                    label="内容"
-                    :rules="[{ required: true, message: '请输入内容' }]"
-                >
-                    <MarkdownEditor
-                        v-model:value="formState.content"
-                        :rows="10"
-                    />
-                </a-form-item>
-
-                <a-form-item :wrapper-col="{ span: 12, offset: 4 }">
+                <a-form-item :wrapper-col="{ span: 8, offset: 6 }">
                     <a-button
                         type="primary"
                         @click="onSubmit"
@@ -31,6 +21,13 @@
                     <a-button style="margin-left: 10px" @click="$router.back()">
                         返回
                     </a-button>
+                </a-form-item>
+
+                <a-form-item :wrapper-col="{ span: 20, offset: 2 }">
+                    <MarkdownEditor
+                        v-model:value="formState.content"
+                        :rows="20"
+                    />
                 </a-form-item>
             </a-form>
         </template>
--- view/src/views/manage/article/Info.vue
+++ view/src/views/manage/article/Info.vue
@@ -1,7 +1,7 @@
 <template>
     <ContentPage>
         <template #content>
-            <a-form :model="formState" :label-col="{ span: 4 }" :wrapper-col="{ span: 12 }">
+            <a-form :model="formState" :label-col="{ span: 6 }" :wrapper-col="{ span: 8 }">
                 <a-form-item label="ID">
                     <a-input v-model:value="formState.id" disabled />
                 </a-form-item>
@@ -10,14 +10,7 @@
                     <a-input v-model:value="formState.title" />
                 </a-form-item>
 
-                <a-form-item label="内容">
-                    <MarkdownEditor
-                        v-model:value="formState.content"
-                        :rows="10"
-                    />
-                </a-form-item>
-
-                <a-form-item :wrapper-col="{ span: 12, offset: 4 }">
+                <a-form-item :wrapper-col="{ span: 8, offset: 6 }">
                     <a-button
                         type="primary"
                         @click="onSubmit"
@@ -29,6 +22,13 @@
                     <a-button style="margin-left: 10px" @click="$router.back()">
                         返回
                     </a-button>
+                </a-form-item>
+
+                <a-form-item :wrapper-col="{ span: 20, offset: 2 }">
+                    <MarkdownEditor
+                        v-model:value="formState.content"
+                        :rows="20"
+                    />
                 </a-form-item>
             </a-form>
         </template>
